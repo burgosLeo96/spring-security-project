95a333192f26f2b3858534cf288b5869e5c36511
Leonard Burgos <burgosleonard96.lb@gmail.com>
2025-04-24 18:24:27 -0500

    Added in-memory users

--- src/main/java/org/burgosleo/springsecurityproject/config/SecurityConfiguration.java
+++ src/main/java/org/burgosleo/springsecurityproject/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package org.burgosleo.springsecurityproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class SecurityConfiguration {
@@ -26,10 +33,33 @@ public class SecurityConfiguration {
                 "/logout",
                 "/error").permitAll());
 
-        http.formLogin(AbstractHttpConfigurer::disable);
+        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .authorities("read").build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$2HN2/rntx1ejOfTwR1L8A.jE0RrQkMWRYkD4fDJgg.e9yjWGLWO3q")
+                .authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 
 }
